933884d64f4d2fba7875d224435eda6bdf14f566
Lucas Vieira <lvieirastos@gmail.com>
2022-02-13 17:08:34 -0300

    add intent

--- app/src/main/java/com/lucasvieira/agenda/MainActivity.kt
+++ app/src/main/java/com/lucasvieira/agenda/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.lucasvieira.agenda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById(R.id.set_event) as Button
+        val textTitle = findViewById(R.id.text_title) as EditText
+        val textLocal = findViewById(R.id.text_local) as EditText
+
+        btnSetEvent.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_INSERT)
+                    .setData(CONTENT_URI)
+                    .putExtra(TITLE, textTitle.text.toString())
+                    .putExtra(EVENT_LOCATION, textLocal.text.toString())
+                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60 * 60 * 1000 ))
+
+            startActivity(intent)
+
+        }
     }
 }
